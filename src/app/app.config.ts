f7e82bde786d87f4cc887859bd3f8840fc0b7a6a
Radad El Jaidi <radad1998@gmail.com>
2025-01-31 15:07:16 +0100

    styling header and footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+//primeng config 
+
 export const appConfig: ApplicationConfig = {
   providers: [ provideRouter(routes), provideHttpClient()]
 };
